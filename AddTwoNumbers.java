711eca8a6b5b89602e70f10059afa065c197b2ab
Anusha <anushab.xworks@gmail.com>
2021-10-30 16:24:00 +0530

    Adding two numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,14 @@
+class ArthematicOperators{
+	public static void main(String args[])
+	{
+		System.out.println("sum:" + (70+30));
+		int sum = 70 + 30;
+		System.out.println("sum is:" +sum);
+		
+		int sub;
+		sub = 70 - 30;
+		System.out.println("sub:" + sub)
+	}
+	
+}
+   
